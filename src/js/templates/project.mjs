84aa51f4c70710da87ba9170e261971c74a15b19
mariesaeter <marie.saeter@gmail.com>
2023-12-17 16:26:56 +0100

    Added details to about me

--- src/js/templates/project.mjs
+++ src/js/templates/project.mjs
@@ -10,8 +10,10 @@ export const displayProjects = () => {
     html += `<div class="snap-align h-100 projectCard">
         <h3>${title}</h3>
         <small>${tools}</small>
-        <img src=${media} alt="${title} screenshot" />
-        <p>${description}</p>
+        <div class="md:grid md:grid-cols-2 md:gap-3">
+        <img class src=${media} alt="${title} screenshot" />
+        <p class="py-3">${description}</p>
+        </div>
        <div class="flex links">
         <a href=${github} class="flex-column align-items-center"><i class="fa-brands fa-github"></i><small>repository</small></a>
         <a href=${netlify} class="flex-column align-items-center"><i class="fa-solid fa-globe"></i><small>netlify</small></a>
